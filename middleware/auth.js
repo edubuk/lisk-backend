b830bc9f3ab9bd8dc2349bf74bf551aff974e283
ajeetram <86039147+ajeetram@users.noreply.github.com>
2024-11-11 20:22:06 +0530

    auth error fixed

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv'
 dotenv.config();
-const secret_key = process.env.secret_key;
+const secret_Key = process.env.secret_key;
 const payloadData = process.env.payloadData;
 export const generateToken =()=> {
 // Set `noTimestamp` to true to exclude `iat`
@@ -13,11 +13,15 @@ console.log("Token : ",nonExpiryToken)
 
 export const authenticateToken = (req,res,next)=>{
     const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
+    console.log("Token value",token);
     if(!token)
         return res.status(401).json({message:'Token Required'});
 
-    jwt.verify(token,secret_key,(err)=>{
-        if(err) return res.status(403).json({message:'Invalid Token'});
+    jwt.verify(token,secret_Key,(err,decoded)=>{
+        console.log("payload data ",decoded)
+        if(err) return res.status(403).json({message:'Invalid Token',
+            error:err
+        });
         next();
     });
 
